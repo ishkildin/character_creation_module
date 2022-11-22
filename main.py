be8f26f68f865fa5cd3f183d59cfaff49735ecd9
Артур Ишкильдин <ishkildin2@yandex.ru>
2022-11-22 16:54:00 +0500

    Add ASCII-graphics animation on the start of the game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
     """Вывод строки о нанесении урона в зависимости от класса персонажа.
@@ -110,6 +111,7 @@ def choice_char_class() -> str:
 
 
 if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
