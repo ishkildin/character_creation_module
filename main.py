31a3160b4df1708207d34d657c67132106183339
Артур Ишкильдин <ishkildin2@yandex.ru>
2022-11-22 16:40:42 +0500

    code according to PEP257

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from random import randint
 
-
 def attack(char_name: str, char_class: str) -> str:
+    """Вывод строки о нанесении урона в зависимости от класса персонажа.
+
+    Функция возвращает строку с информацией на вывод
+    о нанесении урона персонажем.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +19,11 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Вывод строки о блокировке урона в зависимости от класса персонажа.
+
+    Функция возвращает строку с информацией на вывод
+    о примененной блокировке.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +34,11 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Вывод строки о применении суперсилы в зависимости от класса персонажа.
+
+    Функция возвращает строку с информацией на вывод
+    о примененной суперсиле.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +50,13 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Бесконечный цикл тренировки до ввода пользователем команды 'skip'.
+
+    У пользователя запрашивается команда для тренировки:
+    атака, защита, суперсила. После ввода команды 'skip'
+    ожидание ввода с клавиатуры пользователя оканчивается
+    и возвращается строка 'Тренировка окончена.'
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +82,12 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Запрашиваем у пользователя за какой класс он будет играть.
+
+    Название класса запрашиваем с клавиатуры пользователя.
+    Запрашиваем с клавиатуры подтверждение выбора класса.
+    Функция возвращает название класса.
+    """
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -82,7 +109,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,6 +119,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-    return None
-
-main()
